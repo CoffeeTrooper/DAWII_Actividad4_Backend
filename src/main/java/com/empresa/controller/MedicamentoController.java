5b39e788184ce867de3c89ea063000ddb3e89c70
CoffeeTrooper <sofiaalejandrahs18@gmail.com>
2021-09-18 22:36:16 -0500

    Subiendo backend de Actividad4

--- src/main/java/com/empresa/controller/MedicamentoController.java
+++ src/main/java/com/empresa/controller/MedicamentoController.java
@@ -1,12 +1,13 @@
 package com.empresa.controller;
 
+import java.util.HashMap;
 import java.util.List;
-import java.util.Optional;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,9 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.empresa.entity.Medicamento;
 import com.empresa.service.MedicamentoService;
+import com.empresa.util.Constantes;
 
 @RestController
 @RequestMapping("/rest/medicamento")
+
+@CrossOrigin(origins = "http://localhost:4200")
 public class MedicamentoController {
 
 	@Autowired
@@ -31,61 +35,28 @@ public class MedicamentoController {
 		return ResponseEntity.ok(lista);
 	}
 	
-	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@PostMapping
 	@ResponseBody
-	public ResponseEntity<Medicamento> registrarMedicamento(@RequestBody Medicamento med){
-		Medicamento salida = service.registroMedicamento(med);
+	public ResponseEntity<Map<String, Object>> insertaMedicamento(@RequestBody Medicamento obj){
 		
-		if(salida == null) {
-			ResponseEntity.noContent().build();
+		Map<String, Object> salida = new HashMap<>();
+		
+		try {
+			Medicamento objSalida = service.registroMedicamento(obj);
+			
+			if(objSalida == null) {
+				salida.put("mensaje", Constantes.MENSAJE_REG_ERROR);
+			}
+			else {
+				salida.put("mensaje", Constantes.MENSAJE_REG_EXITOSO);
+			}
+		}
+		catch(Exception e) {
+		e.printStackTrace();
+		salida.put("mensaje", Constantes.MENSAJE_REG_ERROR);
 		}
 		
 		return ResponseEntity.ok(salida);
 	}
-	
-	//Métodos Actividad 2
-	
-	//1. Consulta de Medicamento por ID
-	@GetMapping("/{idParam}")
-	@ResponseBody
-	public ResponseEntity<Optional<Medicamento>> buscarMedicamentoPorId(@PathVariable("idParam") int id){
-		Optional<Medicamento> medicamento = service.buscarPorId(id);
-		
-		if(medicamento.isEmpty()) {
-			return ResponseEntity.noContent().build();
-		}
-		else {
-			return ResponseEntity.ok(medicamento);
-		}
-	}
-	
-	//2. Consulta de Medicamento por Nombre use LIke
-	@GetMapping("/nombre/{nom}")
-	@ResponseBody
-	public ResponseEntity<List<Medicamento>> buscarMedicamentoPorNombre(@PathVariable("nom") String nombre){
-		List<Medicamento> medicamento = service.buscarPorNombre(nombre);
-		
-		if(medicamento.isEmpty()) {
-			return ResponseEntity.noContent().build();
-		}
-		else {
-			return ResponseEntity.ok(medicamento);
-		}
-	}
-	
-	//3. Consulta de Medicamento por Stock
-	@GetMapping("/stock/{stock}")
-	@ResponseBody
-	public ResponseEntity<List<Medicamento>> buscarMedicamentoPorStock(@PathVariable("stock") int stock){
-		List<Medicamento> medicamento = service.buscarPorStock(stock);
-		
-		if(medicamento.isEmpty()) {
-			return ResponseEntity.noContent().build();
-		}
-		else {
-			return ResponseEntity.ok(medicamento);
-		}
-	}
-	
 }
